bc09c75b3c2bf890e816f4439e1da2464caf1ba4
guorl <guorl@mac>
2018-05-15 16:27:41 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by guorenlei on 2018/5/15.
+//  Copyright © 2018年 guorenlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
